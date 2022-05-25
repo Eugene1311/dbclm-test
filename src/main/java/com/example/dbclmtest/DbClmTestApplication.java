eded21d052eb575159971bdc47c4b5948447a395
EFedoseev <ef7@mail.ru>
2022-05-25 21:44:15 +0300

    initial commit

---
+++ src/main/java/com/example/dbclmtest/DbClmTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbclmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbClmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbClmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbclmtest/DbClmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbclmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbClmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
