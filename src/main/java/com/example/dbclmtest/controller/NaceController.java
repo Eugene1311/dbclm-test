81edd6d3e22c1e3782b5920cfc17fe0d4d32c83e
EFedoseev <ef7@mail.ru>
2022-06-06 00:01:15 +0300

    batch-insert: add swagger

--- src/main/java/com/example/dbclmtest/controller/NaceController.java
+++ src/main/java/com/example/dbclmtest/controller/NaceController.java
@@ -3,8 +3,14 @@ package com.example.dbclmtest.controller;
 import com.example.dbclmtest.dto.Nace;
 import com.example.dbclmtest.service.NaceService;
 import io.r2dbc.spi.R2dbcDataIntegrityViolationException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,7 +23,13 @@ import reactor.core.publisher.Mono;
 public class NaceController {
     private final NaceService naceService;
 
-    @PostMapping(value = "/upload")
+    @Operation(summary = "Upload file with NACE details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "NACE details are stored",
+                    content = { @Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = @Content) }
+    )
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Mono<Integer> saveNaceDetails(@RequestPart("file") FilePart file) {
         return naceService.saveNaceDetails(file)
                 .onErrorMap(err -> {
@@ -28,15 +40,30 @@ public class NaceController {
                 });
     }
 
+    @Operation(summary = "Get a NACE by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the NACE",
+                content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Nace.class)) }),
+        @ApiResponse(responseCode = "404", description = "NACE not found",
+                content = @Content) }
+    )
     @GetMapping(value = "/{order}")
     public Mono<Nace> getNaceDetailsByOrder(@PathVariable int order) {
         return naceService.getNaceDetailsByOrder(order)
-                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Nace not found")));
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "NACE not found")));
     }
 
+    @Operation(summary = "Get all NACEs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all NACEs",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Nace.class)) }),
+            @ApiResponse(responseCode = "404", description = "NACEs not found",
+                    content = @Content) })
     @GetMapping
-    public Flux<Nace> getNaceDetailsByOrder() {
+    public Flux<Nace> getAllNaceDetails() {
         return naceService.getAllNaceDetails()
-                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Naces not found")));
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "NACEs not found")));
     }
 }
